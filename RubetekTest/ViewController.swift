06ca0cbf6275abf115bb0de2bec9b44fec4a009c
Darya Dainiak <daryadainiak@gmail.com>
2021-09-21 17:26:02 +0300

    first screen in progress

--- RubetekTest/ViewController.swift
+++ RubetekTest/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var cameraTableView: UITableView!
+    private let titleCellId = "title"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpTableView()
     }
-
-
+    
+    
+    private func setUpTableView() {
+        cameraTableView.delegate = self
+        
+        cameraTableView.dataSource = self
+    }
+    
+    
 }
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: titleCellId, for: indexPath) as? CameraCell else { return UITableViewCell() }
+        cell.fill()
+        
+        return cell
+        
+    }
+}
+
 
